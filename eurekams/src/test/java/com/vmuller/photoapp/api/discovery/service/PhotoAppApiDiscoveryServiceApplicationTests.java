64a201c1646c260689e4c3916d5feb5e4b097756
Vinicio Müller <vinicio.muller@outlook.com>
2022-03-02 17:59:01 -0300

    First microservices with eureka server and classes clients

---
+++ eurekams/src/test/java/com/vmuller/photoapp/api/discovery/service/PhotoAppApiDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vmuller.photoapp.api.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
