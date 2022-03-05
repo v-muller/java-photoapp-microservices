4d53ad422c0ca0df1ade1cc7cc2113e162ee24c5
Vinicio Müller <vinicio.muller@outlook.com>
2022-03-05 00:13:14 -0300

    create personal in user class

--- usersapi/src/main/java/com/vmuller/photo/api/users/controllers/UserController.java
+++ usersapi/src/main/java/com/vmuller/photo/api/users/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.vmuller.photo.api.users.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private Environment env;
+
     @GetMapping("/status/check")
     public String status(){
-        return "Working";
+        return "Working" + env.getProperty("local.server.port");
     }
 }
